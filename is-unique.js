281b0f391fa225b3fdd716ed49e02a8c8d3fcbf4
Johnny <po_bo@hotmail.com>
2017-07-21 04:24:58 -0700

    cleaned up code

--- is-unique.js
+++ is-unique.js
@@ -1,21 +1,14 @@
-/*
-implement an algorithm to determin if a string has all unique characters, what if you cannot use additional data structures?
-*/
-
 const isUnique = (string) => {
   let state = true
   let storage = {};
-    for (var i = 0; i < string.length; i++) {
-      console.log('this is string[1]', string[i])
-      console.log('this is storage has property', storage.hasOwnProperty(string[i]))
-      if (storage.hasOwnProperty(string[i])) {
-        return state = false
-      } else {
-        storage[string[i]] = 0;
-        console.log('this is storage', storage)
-      }
+  for (var i = 0; i < string.length; i++) {
+    if (storage.hasOwnProperty(string[i])) {
+      return state = false
+    } else {
+      storage[string[i]] = 0;
     }
+  }
   return state;
 }
 
-console.log(isUnique('world'))
+// console.log(isUnique('wold'));
