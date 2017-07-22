f336570c13a554239ca2c1af5354a82193c0ed00
Johnny <po_bo@hotmail.com>
2017-07-22 00:22:32 -0700

    completes checkPermutation with jasmine test wirtten to test function

--- chapter-one-problems.js
+++ chapter-one-problems.js
@@ -16,5 +16,34 @@ const isUnique = (string) => {
   }
   return state;
 }
+// console.log(isUnique('world'));
 
-console.log(isUnique(''));
+const checkPermutation = (str1, str2) => {
+  let state = true;
+  let storage = {};
+  if (str1.length === 0 || str2.length === 0) {
+    return 'Please input strings with length > 0';
+  }
+  if (str1.length !== str2.length) {
+    return state = false;
+  }
+  for (var i = 0; i < str1.length; i++) {
+    if (!storage.hasOwnProperty(str1[i])) {
+      storage[str1[i]] = 1;
+    } else {
+      storage[str1[i]] = storage[str1[i]] +1;
+    }
+  }
+  for (var j = 0; j < str2.length; j++) {
+    if (!storage.hasOwnProperty(str2[j])) {
+      return state = false;
+    } else {
+      storage[str2[j]] = storage[str2[j]] -1;
+      if (storage[str2[j]] < 0) {
+        return state = false;
+      }
+    }
+  }
+  return state;
+};
+// console.log(checkPermutation('wworwd', 'wworww'));
--- test/spec/test.js
+++ test/spec/test.js
@@ -3,6 +3,10 @@
 
   describe('Chapter 1 coding challenge test', function () {
     describe('question 1.1', function () {
+      it('should return a message to user if the string is empty', function () {
+        expect(isUnique('')).toBe('Please input a string with length > 0');
+      });
+
       it('should check if each characters in given string is unique', function () {
         expect(isUnique('world')).toBe(true);
       });
@@ -11,9 +15,30 @@
         expect(isUnique('woorld')).toBe(false);
       });
 
-      it('should return a message to user if the string is empty', function () {
-        expect(isUnique('')).toBe('Please input a string with length > 0');
+    });
+
+    describe('question 1.2', function () {
+      it('should return message to user if either string is empty', function () {
+        expect(checkPermutation('', '')).toBe('Please input strings with length > 0')
       });
+
+      it('should return false if one string length doesn\'t match the other string length', function () {
+          expect(checkPermutation('woorld', 'wooorld')).toBe(false);
+      });
+
+      it('should return false if str2 does not have letters exist in str1', function (){
+        expect(checkPermutation('woorld', 'weerld')).toBe(false);
+      });
+
+      it('should return false if string 2 is not permutation of string 1', function () {
+        expect(checkPermutation('world', 'woold')).toBe(false);
+      });
+
+      it('should return true if string 2 is permutation of string 1', function () {
+        expect(checkPermutation('worldd', 'orldwd')).toBe(true);
+      });
+
+
     });
   });
 
