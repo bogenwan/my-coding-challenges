1749d4f9e4c9e37bb6e8faada69d8852b01d2542
Johnny <po_bo@hotmail.com>
2017-08-19 07:18:13 -0700

    implemented singlyLinked list with add and remove method

--- chapter-two-problems.js
+++ chapter-two-problems.js
@@ -4,18 +4,75 @@ const Node = function (value) {
   this.next = null;
 };
 
-const removeDub = function (linkedList) {
+const SinglyList = function () {
+  this.length = 0;
+  this.head = null;
+};
+
+SinglyList.prototype.add = function (value) {
+  let node = new Node(value);
+  let currentHead = this.head;
+  if (!currentHead) {
+    this.head = node;
+    this.length++;
+  } else {
+    while(currentHead.next) {
+      currentHead = currentHead.next;
+    }
+    currentHead.next = node;
+    this.length++;
+  }
+};
+
+SinglyList.prototype.remove = function (value) {
+  let currentHead = this.head;
+  if (currentHead.value === value) {
+    this.head = currentHead.next;
+    this.length--;
+  } else {
+    let previousHead = currentHead;
+    while (currentHead.next) {
+      if (currentHead.value === value) {
+        // console.log(currentHead.next)
+        // console.log(previousHead.next)
+        previousHead.next = currentHead.next;
+        // console.log(previousHead.next)
+        this.length--;
+        break;
+      }
+      previousHead = currentHead;
+      currentHead = currentHead.next;
+    }
+    console.log(currentHead)
+    console.log(previousHead)
+    if (currentHead.value === value) {
+      previousHead.next = null;
+      this.length--;
+    }
+  }
+};
+
+SinglyList.prototype.removeDub = function (linkedList) {
 return linkedList;
 };
 
-let a = new Node(1);
-let b = new Node(2);
-let c = new Node(3);
-let d = new Node(4);
-let e = new Node(3);
-a.next = b;
-b.next = c;
-c.next = d;
-d.next = e;
-console.dir(c)
-console.log(removeDub('johnny'));
+const testListOne = new SinglyList();
+testListOne.add(1);
+testListOne.add(2);
+testListOne.add(3);
+testListOne.remove(3);
+// SinglyList.add(1);
+// SinglyList.add(2);
+// SinglyList.add(3);
+console.log('testListOne ==>', testListOne)
+// let a = new Node(1);
+// let b = new Node(1);
+// let c = new Node(2);
+// let d = new Node(4);
+// let e = new Node(3);
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+// console.log(a)
+// console.log(removeDub('johnny'));
