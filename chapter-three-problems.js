b3261fcce04fa54f413b8284e38d583c0f25154c
Johnny <po_bo@hotmail.com>
2017-08-31 23:28:18 -0700

    implemented triple stack with add method with ES6 class, will continue implament pop and peek method.

--- chapter-three-problems.js
+++ chapter-three-problems.js
@@ -1,14 +1,76 @@
-const Stack = function () {
-  this.length = 0;
-  this.storage = {};
+class Stack {
+  constructor () {
+    this.storage = {};
+    this.length = 0;
+  };
+
+  push (value) {
+    this.storage[this.length] = value;
+    this.length++;
+  };
+
+  pop () {
+    delete this.storage[this.length -1];
+    if (this.length >= 1) {
+      this.length--;
+    };
+  };
+
+  size () {
+    return this.length;
+  };
+
+  peek () {
+    return this.storage[this.length -1];
+  };
+};
+// var testStack = new Stack();
+// testStack.push('a');
+// testStack.push('b');
+// testStack.pop();
+// testStack.pop();
+// testStack.size();
+// console.log(testStack.peek());
+class tripleStack {
+  constructor (size) {
+    this.storage = new Array(3 * size);
+    this.length = size;
+    this.stackTracker = {};
+  };
+
+  push (stackNum, value) {
+    if (stackNum > this.length) {
+      console.log('Exceed stack container limit!');
+      return;
+    }
+    let currStack = (stackNum * 3) - 3;
+    if (this.stackTracker[currStack]) {
+      if (this.stackTracker[currStack] < 3) {
+        let currStackPointer = this.stackTracker[currStack];
+        this.storage[currStack + currStackPointer] = value;
+        this.stackTracker[currStack] = currStackPointer + 1 ;
+      } else {
+        console.log('This stack is full, please try another stack!');
+        return;
+      }
+    }
+    else {
+      this.storage[currStack] = value;
+      this.stackTracker[currStack] = 1;
+    };
+  };
 };
 
-Stack.prototype.push = function (value) {
-  this.length++;
-  this.storage[this.length] = value;
-}
-
-var testStack = new Stack();
-testStack.push(3);
-testStack.push(4);
-console.log(testStack)
+var es6Stack = new tripleStack(4);
+es6Stack.push(1, 'a');
+es6Stack.push(1, 'b');
+es6Stack.push(1, 'c');
+es6Stack.push(2, 'd');
+es6Stack.push(2, 'e');
+es6Stack.push(2, 'f');
+es6Stack.push(3, 'g');
+es6Stack.push(4, 'h');
+es6Stack.push(4, 'hi');
+es6Stack.push(4, 'hiii');
+es6Stack.push(4, 'hiiii');
+console.log(es6Stack);
--- chapter-three-test/spec/test.js
+++ chapter-three-test/spec/test.js
@@ -2,7 +2,7 @@
   'use strict';
 
   describe('Chapter 3 coding challenge test', function () {
-
+    let stack1 = new Stack();
     describe('Question 3.1', function () {
       it('Should implement three stack with an array', function () {
         expect(true).toBe(true);
