78bba75a8f61cb43048fb510a43eb6b92943776a
Johnny <po_bo@hotmail.com>
2017-09-02 04:06:19 -0700

    implemented stack with min functions for push, pop and peekMin, will write test

--- chapter-three-problems.js
+++ chapter-three-problems.js
@@ -1,41 +1,7 @@
-class Stack {
-  constructor () {
-    this.storage = {};
-    this.length = 0;
-  };
-
-  push (value) {
-    this.storage[this.length] = value;
-    this.length++;
-  };
-
-  pop () {
-    delete this.storage[this.length -1];
-    if (this.length >= 1) {
-      this.length--;
-    }
-  };
-
-  size () {
-    return this.length;
-  };
-
-  peek () {
-    return this.storage[this.length -1];
-  };
-};
-// var testStack = new Stack();
-// testStack.push('a');
-// testStack.push('b');
-// testStack.pop();
-// testStack.pop();
-// testStack.size();
-// console.log(testStack.peek());
 
 class tripleStack {
   constructor (size) {
     this.storage = [];
-    // this.length = size;
     this.stackTracker = {};
   };
 
@@ -91,31 +57,92 @@ class tripleStack {
   };
 };
 
-var es6Stack = new tripleStack(4);
-es6Stack.push(1, 'a');
-es6Stack.push(1, 'b');
-es6Stack.push(1, 'c');
-es6Stack.push(2, 'd');
-es6Stack.push(2, 'e');
-es6Stack.push(2, 'f');
-es6Stack.push(3, 'g');
-es6Stack.push(4, 'h');
-es6Stack.push(4, 'hi');
-es6Stack.push(4, 'hiii');
+// var es6Stack = new tripleStack(4);
+// es6Stack.push(1, 'a');
+// es6Stack.push(1, 'b');
+// es6Stack.push(1, 'c');
+// es6Stack.push(2, 'd');
+// es6Stack.push(2, 'e');
+// es6Stack.push(2, 'f');
+// es6Stack.push(3, 'g');
+// es6Stack.push(4, 'h');
+// es6Stack.push(4, 'hi');
+// es6Stack.push(4, 'hiii');
 // es6Stack.push(4, 'hiiii');
-es6Stack.push(3, 'jk');
-es6Stack.push(3, 'et');
-es6Stack.pop(2);
-es6Stack.pop(2);
-es6Stack.pop(2);
+// es6Stack.push(3, 'jk');
+// es6Stack.push(3, 'et');
+// es6Stack.pop(2);
+// es6Stack.pop(2);
+// es6Stack.pop(2);
 // es6Stack.peek(1);
-es6Stack.pop(1);
+// es6Stack.pop(1);
 // es6Stack.peek(1);
-es6Stack.pop(1);
+// es6Stack.pop(1);
 // es6Stack.peek(1)
-es6Stack.pop(1);
+// es6Stack.pop(1);
 // es6Stack.peek(1);
 // es6Stack.push(1, 'z');
 // es6Stack.popAll();
 
 // console.log(es6Stack);
+
+class Stack {
+  constructor () {
+    this.storage = {};
+    this.minStorage = {};
+    this.minStoreIndex = 0;
+    this.length = 0;
+  };
+
+  push (value) {
+    this.storage[this.length] = value;
+    this.length++;
+    if (!this.minStorage[this.minStoreIndex]) {
+      this.minStorage[this.minStoreIndex] = value;
+    } else if (value <= this.minStorage[this.minStoreIndex]) {
+      this.minStoreIndex++;
+      this.minStorage[this.minStoreIndex] = value;
+    }
+  };
+
+  pop () {
+    let poped = this.storage[this.length -1];
+    delete this.storage[this.length -1];
+    if (this.length >= 1) {
+      this.length--;
+    }
+    if (poped === this.minStorage[this.minStoreIndex]) {
+      delete this.minStorage[this.minStoreIndex];
+      if (this.minStoreIndex > 0) {
+        this.minStoreIndex--;
+      }
+    }
+  };
+
+  size () {
+    return this.length;
+  };
+
+  peek () {
+    return this.storage[this.length -1];
+  };
+
+  peekMin () {
+    return this.minStorage[this.minStoreIndex];
+  };
+};
+var testStack = new Stack();
+testStack.push(7);
+testStack.push(5);
+testStack.push(8);
+testStack.push(9);
+testStack.push(5);
+testStack.push(2);
+// testStack.pop();
+// testStack.pop();
+// testStack.pop();
+// testStack.pop();
+// testStack.pop();
+// testStack.pop();
+console.log(testStack.peekMin());
+console.log(testStack);
